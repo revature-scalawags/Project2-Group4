50343c8e6d89c6ebedb656560067fdbede2838df
Rastal <rastal@protonmail.com>
2021-01-17 13:06:36 -0800

    Tweak historical scraper for testing

--- historical-scraper/src/main/scala/RestExample.scala
+++ historical-scraper/src/main/scala/RestExample.scala
@@ -1,15 +1,19 @@
 import com.danielasfregola.twitter4s.TwitterRestClient
+import com.danielasfregola.twitter4s.entities.{HashTag, Tweet}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object RestExample extends App {
 
-  // Make sure to define the following env variables:
-  // TWITTER_CONSUMER_TOKEN_KEY and TWITTER_CONSUMER_TOKEN_SECRET
-  // TWITTER_ACCESS_TOKEN_KEY and TWITTER_ACCESS_TOKEN_SECRET
-  val restClient = TwitterRestClient()
+  // Make sure to define consumer and access tokens
+  val client = TwitterRestClient()
+  val user = "therastal"
+  val limit = 10
 
-  restClient.homeTimeline().map { ratedData =>
-    val tweets = ratedData.data
-    tweets.foreach(tweet => println(tweet.text))
+  val results = client
+    .userTimelineForUser(screen_name = user, count = limit)
+    .map { ratedData =>
+      val tweets = ratedData.data
+      println(s"${user.toUpperCase}'S LATEST TWEETS:")
+      println(tweets.mkString("\n-----------\n"))
   }
 }
