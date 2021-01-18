4bf9c5892c64f657c4409e9c72cce42342f187e2
Rastal <rastal@protonmail.com>
2021-01-18 12:07:21 -0800

    Change scraper from user to hashtag-based

--- historical-scraper/src/main/scala/HistoricalScraper.scala
+++ historical-scraper/src/main/scala/HistoricalScraper.scala
@@ -1,4 +1,5 @@
 import com.danielasfregola.twitter4s.entities.{HashTag, Tweet}
+import com.danielasfregola.twitter4s.entities.enums.ResultType
 import com.danielasfregola.twitter4s.TwitterRestClient
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -6,17 +7,19 @@ object HistoricalScraper extends App {
 
   // Make sure to define consumer and access tokens
   val client = TwitterRestClient()
-  val user = "nintendoamerica"
+  val hashtag = "savemelee"
   val limit = 10
+  val recentOrPopular = ResultType.Recent
 
-  val results = client
-    .userTimelineForUser(screen_name = user, count = limit)
-    .map { ratedData =>
-      val tweets = ratedData.data
-      println(s"${user.toUpperCase}'S LATEST TWEETS:")
-      println(tweets.mkString("\n-----------\n"))
+  for (i <- 0 until args.length) {
+    val results = client
+      .searchTweet(s"#$hashtag", count = limit, result_type = recentOrPopular)
+      .map { ratedData => 
+        val tweets = ratedData.data.statuses
+        tweets.foreach(tweet => println(s"\n${tweet.text}"))
+      }
   }
 
-  Thread.sleep(5000)
+  Thread.sleep(3000)
   client.shutdown()
 }
